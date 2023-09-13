0bd7aa719353f420d34b072677ee51f232ea25bc
Marie Angelique <mmembrido@sandiego.edu>
2023-09-12 23:55:15 -0700

    Implemented Admin class with add/remove teacher, view/remove lesson plan functionalities

---
+++ src/main/java/org/example/Admin.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Admin {
+    private List<Teacher> teachers;
+    private List<LessonPlan> lessonPlans;
+
+    public Admin() {
+        this.teachers = new ArrayList<>();
+        this.lessonPlans = new ArrayList<>();
+    }
+
+    // Add new teacher specifying first name, last name, subject
+    public boolean addTeacher(String firstName, String lastName, String subject) {
+        if (firstName != null && lastName != null && subject != null) {
+            Teacher newTeacher = new Teacher(firstName, lastName, subject);
+            teachers.add(newTeacher);
+            return true;
+        }
+        return false;
+    }
+
+    // Remove teacher by first name and last name
+    public boolean removeTeacher(String firstName, String lastName) {
+        for (Teacher t : teachers) {
+            if (t.getFirstName().equals(firstName) && t.getLastName().equals(lastName)) {
+                teachers.remove(t);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // View all lesson plans
+    public List<LessonPlan> viewAllLessonPlans() {
+        return new ArrayList<>(lessonPlans);
+    }
+
+    // Remove a lesson plan
+    public boolean removeLessonPlan(String subject, String title) {
+        for (LessonPlan lp : lessonPlans) {
+            if (lp.getSubject().equals(subject) && lp.getTitle().equals(title)) {
+                lessonPlans.remove(lp);
+                return true;
+            }
+        }
+        return false;
+    }
+}
